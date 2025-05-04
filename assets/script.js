b7d6b02065764114b118cc46495cc8ebec0517b0
Noumouni Traore <Traorenoumouni8@gmail.com>
2025-05-04 09:45:27 +0200

    <heart>

--- assets/script.js
+++ assets/script.js
@@ -17,17 +17,44 @@ const slides = [
 	}
 ]
 
-const left = document.querySelector (".arrow_left"); 
 
-const right = document.querySelector ( ".arrow_right"); 
+slides.length;
+console.log(slides)
+
+let index = 0;
+
+const flecheDroite = document.querySelector(".arrow_right"); 
+const flecheGauche = document.querySelector(".arrow_left"); 
+
+const points = document.querySelectorAll(".dot");
+const imageElement = document.querySelector(".banner-img");
+const paragraphe = document.getElementById("paragraph");
+
+function majSlide() {
+	imageElement.src = `./assets/images/slideshow/${slides[index].image}`;
+	paragraphe.innerHTML = slides[index].tagLine;
+	points.forEach(p => p.classList.remove("dot_selected"));
+	points[index].classList.add("dot_selected");
+}
+
+flecheDroite.addEventListener('click', () => {
+	console.log("j'ai cliqué sur la flèche droite");
+	index++;
+	if (index >= slides.length) {
+		index = 0;
+	}
+	majSlide();
+});
+
+flecheGauche.addEventListener('click', () => {
+	console.log("j'ai cliqué sur la flèche gauche");
+	index--;
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+	majSlide();
+});
 
 
-left.addEventListener ('click', () =>  {
-    console.log( "j'ai cliqué sur la flèche left ")
-})
-
-left.addEventListener ('click', () => {
-console.log( " j'ai cliqué sur la flèche right")
-} )
 
 
